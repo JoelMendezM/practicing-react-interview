0f1bf8cd1d705efc55d11e1d822243dcd68e8df5
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-01-31 10:30:52 -0300

    cleaning code

--- src/Service/getRandomImage.js
+++ src/Service/getRandomImage.js
@@ -4,20 +4,4 @@ export const getRandomImage = async (firstWord) => {
     const data = await response.json()
     const { url } = data;
     return url
-  // fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-  // .then(res => res.json())
-  // .then(response => {
-  //   const { url } = response
-  //   setRandomUrlImage(url)
-  //   console.log(url,'url Img api')
-  // })
-}
-
-
-
-export const getRandomFact = async () => {
-  const response = await fetch(CAT_FACT_URL);
-  const data = await response.json();
-  const { fact } = data;
-  return fact;
 }
